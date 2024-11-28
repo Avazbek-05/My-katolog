118eea39c691450227aa4b89fc7df0a29a4b1e63
Avaz <avazbekrozimboyev08@gmail.com>
2024-11-28 14:11:37 +0500

    javascrip masala va figma

---
+++ main.js
@@ -0,0 +1,31 @@
+//1-masala
+
+// let son = "hello";
+// let re = "";
+// for (let i = 0; i <= son.length - 1; i++) {
+//   re += son[i] + "";
+// }
+// console.log(re);
+
+//2-masala
+// let soz1 = "abc";
+// let soz2 = "cba";
+// let soz3 = "";
+// for (let i = soz2.length - 1; i >= 0; i--) {
+//   soz3 += soz2[i];
+// }
+// if (soz3 == soz1) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+
+// 3-masala
+
+// let son = "hello";
+// let re = "";
+// for (let i = 0; i <= son.length - 1; i++) {
+//   re += son[i] + son[i];
+// }
+// console.log(re);
